43abeab50fc03617cdd0d72dcfa3e2ea732acb00
niaim2020 <nyaim2020@gmail.com>
2020-07-10 14:44:01 +0300

    Добавил задание 7 (обработка исключений)

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,27 +8,34 @@ public class Calculator {
         float secondNum = Number.getFloat();
         char operationSymbol = Operations.getOperation();
 
-        float result;
-        switch (operationSymbol) {
-            case '+':
-                System.out.println(String.valueOf(operation.addition(firstNum, secondNum)));
-                result = operation.addition(firstNum, secondNum);
-                break;
-            case '-':
-                System.out.println(String.valueOf(operation.subtraction(firstNum, secondNum)));
-                result = operation.subtraction(firstNum, secondNum);
-                break;
-            case '/':
-                System.out.println(String.valueOf(operation.division(firstNum, secondNum)));
-                result = operation.division(firstNum, secondNum);
-                break;
-            case '*':
-                System.out.println(String.valueOf(operation.multiplication(firstNum, secondNum)));
-                result = operation.multiplication(firstNum, secondNum);
-                break;
-            default:
-                System.out.println("Неизвестная операция. Повторите попытку.");
-                break;
+        try {
+            float result;
+            switch (operationSymbol) {
+                case '+':
+                    System.out.println(String.valueOf(operation.addition(firstNum, secondNum)));
+                    result = operation.addition(firstNum, secondNum);
+                    break;
+                case '-':
+                    System.out.println(String.valueOf(operation.subtraction(firstNum, secondNum)));
+                    result = operation.subtraction(firstNum, secondNum);
+                    break;
+                case '/':
+                    System.out.println(String.valueOf(operation.division(firstNum, secondNum)));
+                    if (secondNum == 0) {
+                        throw new ArithmeticException();
+                    }
+                    result = operation.division(firstNum, secondNum);
+                    break;
+                case '*':
+                    System.out.println(String.valueOf(operation.multiplication(firstNum, secondNum)));
+                    result = operation.multiplication(firstNum, secondNum);
+                    break;
+                default:
+                    System.out.println("Неизвестная операция. Повторите попытку.");
+                    break;
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на 0 невозможно " + e);
         }
     }
 }
